e488a41a4f1aa5d232e27ce058ebb1f75e4fa733
Johnson Sun <j3.soon777@gmail.com>
2025-03-09 22:58:51 +0800

    feat(core): Output optimization score in exported CSV

--- core/nurse_scheduling/exporter.py
+++ core/nurse_scheduling/exporter.py
@@ -6,7 +6,12 @@ from .context import Context
 def get_people_versus_date_dataframe(ctx: Context, solver):
     # Initialize dataframe with size including leading rows and columns
     n_leading_rows, n_leading_cols = 2, 1
-    df = pd.DataFrame("", index=range(n_leading_rows + len(ctx.people)), columns=range(n_leading_cols + len(ctx.dates)))
+    n_trailing_rows, n_trailing_cols = 1, 0
+    df = pd.DataFrame(
+        "",
+        index=range(n_leading_rows + len(ctx.people) + n_trailing_rows),
+        columns=range(n_leading_cols + len(ctx.dates) + n_trailing_cols)
+    )
 
     # Fill day numbers and weekdays
     # - row 0 contains day number
@@ -25,5 +30,9 @@ def get_people_versus_date_dataframe(ctx: Context, solver):
         if solver.Value(ctx.shifts[(d, r, p)]) == 1:
             assert df.iloc[n_leading_rows+p, n_leading_cols+d] == ""
             df.iloc[n_leading_rows+p, n_leading_cols+d] = ctx.requirements[r].id
+
+    # Fill objective value
+    df.iloc[n_leading_rows + len(ctx.people), 0] = "Score"
+    df.iloc[n_leading_rows + len(ctx.people), 1] = solver.Value(ctx.objective)
 
     return df
