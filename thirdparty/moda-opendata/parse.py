0dd71e09e326fa0cac1c2d692f7cfa2afb99ff9c
Johnson Sun <j3.soon777@gmail.com>
2025-04-04 11:31:48 +0800

    feat(core): Add workday and freeday support for 2023~2024

--- thirdparty/moda-opendata/parse.py
+++ thirdparty/moda-opendata/parse.py
@@ -9,19 +9,24 @@ def parse_special_date_info(csv_file_path):
     return special_date_info
 
 def main():
-    filename = "114年中華民國政府行政機關辦公日曆表.csv"
-    # Get the directory of the current script
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    # Construct the path to the CSV file
-    csv_file_path = os.path.join(script_dir, filename)
-    # Parse the holiday calendar
-    special_date_info = parse_special_date_info(csv_file_path)
-    
-    # Print the results
-    print(f"Found {len(special_date_info)} special date entries:")
-    for date, description, is_holiday in special_date_info:
-        # Convert the date string to a datetime object
-        print(f"('{datetime.strptime(str(date), '%Y%m%d').date()}', '{description}', {is_holiday}),")
+    filenames = [
+        "112年中華民國政府行政機關辦公日曆表.csv",
+        "113年中華民國政府行政機關辦公日曆表.csv",
+        "114年中華民國政府行政機關辦公日曆表.csv",
+    ]
+    for filename in filenames:
+        # Get the directory of the current script
+        script_dir = os.path.dirname(os.path.abspath(__file__))
+        # Construct the path to the CSV file
+        csv_file_path = os.path.join(script_dir, filename)
+        # Parse the holiday calendar
+        special_date_info = parse_special_date_info(csv_file_path)
+        
+        # Print the results
+        print(f"Found {len(special_date_info)} special date entries:")
+        for date, description, is_holiday in special_date_info:
+            # Convert the date string to a datetime object
+            print(f"('{datetime.strptime(str(date), '%Y%m%d').date()}', '{description}', {is_holiday}),")
 
 if __name__ == "__main__":
     main()
--- thirdparty/moda-opendata/print_holidays.py
+++ thirdparty/moda-opendata/print_holidays.py
@@ -1,4 +1,5 @@
 import holidays
 
-for date, name in sorted(holidays.TW(years=2025, categories=holidays.PUBLIC).items()):
-    print(date, name)
+for year in range(2023, 2025):
+    for date, name in sorted(holidays.TW(years=year, categories=holidays.PUBLIC).items()):
+        print(date, name)
