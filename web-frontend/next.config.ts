b80f0d66c5496fa0e441804cb5bcb845f31ad235
Johnson Sun <j3.soon777@gmail.com>
2025-08-14 05:10:05 +0800

    ci(web-frontend): Add basepath for github pages

--- web-frontend/next.config.ts
+++ web-frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export',
+  basePath: process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}` : '',
 };
 
 export default nextConfig;
