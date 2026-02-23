155830235f95a6f00812ac25894ce5331de45121
Johnson Sun <j3.soon777@gmail.com>
2026-02-24 00:10:47 +0800

    docs(core): Add module doc and move it above license header for better IDE integration

--- core/nurse_scheduling/constants.py
+++ core/nurse_scheduling/constants.py
@@ -1,23 +1,22 @@
-"""
-This file is part of Nurse Scheduling Project, see <https://github.com/j3soon/nurse-scheduling>.
+"""Shared constants for the nurse scheduling package."""
 
-Copyright (C) 2023-2026 Johnson Sun
+# This file is part of Nurse Scheduling Project, see <https://github.com/j3soon/nurse-scheduling>.
+#
+# Copyright (C) 2023-2026 Johnson Sun
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as
-published by the Free Software Foundation, either version 3 of the
-License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
-# Shared constants for the nurse scheduling module.
 from enum import Enum
 
 from .workdays.taiwan import is_freeday as is_freeday_TW
