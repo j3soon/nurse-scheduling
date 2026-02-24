73bac97c221f578c3a3af7a9790dd3ce3c3a7fb3
Johnson Sun <j3.soon777@gmail.com>
2026-02-25 02:45:27 +0800

    chore(web-frontend): Fix `next lint` deprecation in Next 16
    via `npx @next/codemod@latest next-lint-to-eslint-cli .`
    
    Ref: https://nextjs.org/blog/next-15-5#next-lint-deprecation

--- web-frontend/eslint.config.mjs
+++ web-frontend/eslint.config.mjs
@@ -1,26 +1,22 @@
+import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
+import nextTypescript from "eslint-config-next/typescript";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    rules: {
-      // Require files to end with a newline
-      "eol-last": ["error", "always"],
-      // Disallow trailing whitespace at the end of lines
-      "no-trailing-spaces": "error",
-      // Allow unused parameters that start with underscore
-      "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
-    },
+const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
+  rules: {
+    // Require files to end with a newline
+    "eol-last": ["error", "always"],
+    // Disallow trailing whitespace at the end of lines
+    "no-trailing-spaces": "error",
+    // Allow unused parameters that start with underscore
+    "@typescript-eslint/no-unused-vars": ["error", { "argsIgnorePattern": "^_" }],
   },
-];
+}, {
+  ignores: ["node_modules/**", ".next/**", "out/**", "build/**", "next-env.d.ts"]
+}];
 
 export default eslintConfig;
