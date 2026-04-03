993f2b37a5168ab5dacc21dc9cca69f8f53b799b
Johnson Sun <j3.soon777@gmail.com>
2026-04-03 21:26:14 +0800

    feat: Update and fix holiday parser

--- thirdparty/moda-opendata/print_holidays.py
+++ thirdparty/moda-opendata/print_holidays.py
@@ -1,5 +1,5 @@
 import holidays
 
-for year in range(2023, 2025):
+for year in range(2023, 2027):
     for date, name in sorted(holidays.TW(years=year, categories=holidays.PUBLIC).items()):
         print(date, name)
