82c52aace02b80e3f96064faa365eacac6521dcf
Johnson Sun <j3.soon777@gmail.com>
2026-05-22 03:14:52 +0800

    chore(sentry): Update org and project name

--- web-frontend/next.config.ts
+++ web-frontend/next.config.ts
@@ -31,9 +31,9 @@ export default withSentryConfig(nextConfig, {
   // For all available options, see:
   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
 
-  org: "none-32n",
+  org: "j3soon",
 
-  project: "javascript-nextjs",
+  project: "nurse-scheduling",
 
   // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
